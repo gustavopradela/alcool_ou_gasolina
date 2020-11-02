84321d0b36f0cb168cbf041561cbb78119ed8d78
Gustavo pradela <gustavopradela@gmail.com>
2020-11-02 18:48:46 -0300

    segundo commit

---
+++ lib/TelaSecundaria.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TelaSecundaria extends StatefulWidget {
+  @override
+  _TelaSecundariaState createState() => _TelaSecundariaState();
+}
+
+class _TelaSecundariaState extends State<TelaSecundaria> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
